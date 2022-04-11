8af55f5d399b9f64ede5a93ae788d6b5910f550e
MaxVanGastel <max.vangastel@student.uhasselt.be>
2022-04-11 15:06:09 +0200

    rode driehoek comment

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ bool wireframing = false;
 // Fragment shader zorgt voor de kleur 
 const char *vertexShaderSource ="#version 330 core\n"
     "layout (location = 0) in vec3 aPos;\n"
-    "out vec4 vertexColor;\n"
+    "out vec4 vertexColor;\n" //twee in en out met dezelfde namen worden gelinked door opengl bij linking
     "void main()\n"
     "{\n"
     "   gl_Position = vec4(aPos, 1.0);\n"
